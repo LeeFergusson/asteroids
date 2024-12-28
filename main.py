752481253b56afa49e3e4a0291c9997059921413
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-28 20:57:50 +0000

    feat: implement player class and integrate into main game loop

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""Main file for the asteroids game."""
 import pygame
+from player import Player
 from constants import *
 
 def main():
+    """Main function for the game."""
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -9,8 +12,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
