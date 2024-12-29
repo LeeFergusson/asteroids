02585cd33541adc2bb7671144514b28be6015e89
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-29 12:47:12 +0000

    feat: add asteroid and asteroid field classes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Main file for the asteroids game."""
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
 from constants import *
 
@@ -15,9 +17,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
 
     while True:
