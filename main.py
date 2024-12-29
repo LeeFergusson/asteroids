1dd896e84b24ff7371cd8c043e21cfd89e521db3
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-29 12:24:33 +0000

    feat: implement sprite groups for updatable and drawable entities

--- main.py
+++ main.py
@@ -12,12 +12,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
-        player.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
